09816feeef2489f700924520303f05ca8a902bdd
Bernard Ayamboro <bernardayam493@gmail.com>
2025-01-08 23:30:51 +0000

    Bug fix for search pets and open ai assistant

--- controllers/SearchPetsController/SearchPetsController.js
+++ controllers/SearchPetsController/SearchPetsController.js
@@ -7,7 +7,7 @@ const knex = require("knex")(require("../../knexfile"));
 // Get pets based on input
 const getRecommendedPets = async (req, res) => {
   try {
-    const { userSearchInput } = req.body.searchValue;
+    const  userSearchInput  = req.body.searchValue;
     // Validate body
     const userRecommendationSchema = Joi.object({
       userSearchInput: Joi.string(),
@@ -29,7 +29,7 @@ const getRecommendedPets = async (req, res) => {
         try {
           // Get breed recommendations from OpenAI based on searchValue and cached breeds
           const suggestedBreeds = await generateDogBreedsByOpenAi(
-            { userSearchInput },
+            userSearchInput,
             availableBreedsCache
           );
 
